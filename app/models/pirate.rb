5d241eed40d85007195bb46d71612b182c0c6895
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-26 19:52:11 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+  # attr_accessor :name, :weight, :height
+  # @@all = []
+  # def initialize(name, weight, height)
+    
+  #   @name = name
+  #   @weight = weight
+  #   @height = height
+  #   @@all << self
+  # end
+  
+  # self.all
+  #   @@all
+  # end
+  
+  # self.clear
+  #   @@all = []
+  # end
 end
