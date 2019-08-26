a33b54acc994abd73718e42e1b85ee2081efe9f4
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-26 20:57:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,8 +15,22 @@ module FormsLab
   
   post "/pirates" do
      @pirates = Pirate.new(params[:pirate])
+    params[:pirate][:ships].each do |details|
+      Ship.new(details)
+    end
+    @ships = Ship.all
     
     erb :'pirates/show'
   end
   end
 end
+
+# <h3><%= @pirates.ships[1].name %></h3>
+# <h3><%= @pirates.ships[1].type %></h3>
+# <h3><%= @pirates.ships[1].booty %></h3>
+
+# <h3><%= @pirates.weight %></h3>
+# <h3><%= @pirates.height %></h3>
+# <h3><%= @pirates.ships[0].name %></h3>
+# <h3><%= @pirates.ships[0].type %></h3>
+# <h3><%= @pirates.ships[0].booty %></h3>
